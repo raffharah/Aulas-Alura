7d437f3d143ce293a1be11d629334ea42ac6d252
raffharah <japa.rafael.hara@gmail.com>
2024-07-03 11:09:49 -0300

    troca de var_dump para echo

--- string1.php
+++ string1.php
@@ -8,33 +8,33 @@ $strPontoPos = strpos($email, '.');
 
 //verifica tamanho de senha
 $senha = mb_strlen($password); // uso de mb_ para confirmar tamanho de byte de caracteres especiais
-var_dump($senha);
+echo $senha;
 
 // VERIFICA E SEPARA USER EMAIL DO '@'
 $user = substr($email, 0, $strPosition);
-var_dump(mb_strtoupper($user));
-var_dump(substr($email, $strPosition+1));
+echo mb_strtoupper($user) . PHP_EOL;
+echo substr($email, $strPosition+1) . PHP_EOL;
 
 //remover '.' entre rafael.hara
 $userPonto = substr($email, 0, $strPontoPos);
 //$userPonto = substr($email, $strPontoPos+1);
-var_dump($userPonto);
+echo $userPonto . PHP_EOL;
 
 //separação de string
 var_dump(list ($nome, $sobrenome) = explode(' ', $nome));
-var_dump("o seu nome é $nome $sobrenome");
+echo "o seu nome é $nome $sobrenome" . PHP_EOL;
 
 //Separar inicio de email em nome e sobrenome
 $mail = $email;
 //separa inicio do '@'
 $mail = (list($mailNome, $mailResto) = explode('@', $mail));
-var_dump($mailNome, $mailResto);
+echo $mailNome, $mailResto . PHP_EOL;
 //pega o inicio e separa do . deixando o nome separado
 $mailNome = (list($nomeEmail, $sobrenomeEmail) = explode('.', $mailNome));
-var_dump($nomeEmail, $sobrenomeEmail);
+echo "$nomeEmail $sobrenomeEmail" . PHP_EOL;
 //sera o '@' do .com.br para saber o provedor de email
 $mailResto = (list($mailProvedor, $mailPontoCom, $mailPais) = explode('.', $mailResto));
-var_dump($mailProvedor, $mailPontoCom, $mailPais);
+echo "$mailProvedor.$mailPontoCom.$mailPais" . PHP_EOL;
 
 /*$emails = ['rafael.hara@forseti.com.br', 'rafael.hara@gmail.com', 'rafael.hara@hotmail.com'];
 //verificação de email
